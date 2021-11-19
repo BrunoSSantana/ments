7e881cffccb67442e286e4d4994f487450e23cd6
BrunoSSantana <brunoosouza15@gmail.com>
2021-11-18 21:11:13 -0300

    include mentoreds in ListAllMentorsService

--- src/modules/Mentor/useCases/ListAllMentorsUseCase/ListAllMentorsService.ts
+++ src/modules/Mentor/useCases/ListAllMentorsUseCase/ListAllMentorsService.ts
@@ -3,7 +3,9 @@ import { Mentor, PrismaClient } from '.prisma/client'
 class ListAllMentorsService {
   constructor(private repository: PrismaClient) {}
   async execute(): Promise<Mentor[]> {
-    const allMentors = await this.repository.mentor.findMany()
+    const allMentors = await this.repository.mentor.findMany({
+      include: { mentoreds: true }
+    })
     return allMentors
   }
 }
