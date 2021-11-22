1214c848cd84026c2218bf89c85bf6e1ffea1e97
BrunoSSantana <brunoosouza15@gmail.com>
2021-11-22 12:05:55 -0300

    separate app from server

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,5 @@
-import 'dotenv'
-import cors from 'cors'
-import express, { NextFunction, Request, Response } from 'express'
-import 'express-async-errors'
-
-import { AppError } from './errors/AppErrors'
-import { routes } from './routes'
+import { app } from './app'
 
 const port = process.env.PORT || 3003
-
-const app = express()
-app.use(express.json())
-app.use(cors())
-
-app.use('/api', routes)
-
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message
-      })
-    }
-    return response.status(500).json({
-      status: 'error',
-      message: `Internal server error - ${err.message}`
-    })
-  }
-)
 
 app.listen(port, () => console.log('Template Run!'))
