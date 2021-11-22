5b87d74670f9e0be8c22ccb989d4570c8715996a
BrunoSSantana <brunoosouza15@gmail.com>
2021-11-22 14:12:14 -0300

    add type createMentirFactory

--- src/modules/Mentor/useCases/CreateMentorUseCase/CreateMentorFactory.ts
+++ src/modules/Mentor/useCases/CreateMentorUseCase/CreateMentorFactory.ts
@@ -2,7 +2,7 @@ import { PrismaMentorsRepository } from '../../../../repositories/prisma/PrismaM
 import { CreateMentorController } from './CreateMentorController'
 import { CreateMentorService } from './CreateMentorService'
 
-export const createMentorFactory = () => {
+export const createMentorFactory = (): CreateMentorController => {
   const mentorsRepository = new PrismaMentorsRepository()
   const createMentor = new CreateMentorService(mentorsRepository)
   const createMentorController = new CreateMentorController(createMentor)
