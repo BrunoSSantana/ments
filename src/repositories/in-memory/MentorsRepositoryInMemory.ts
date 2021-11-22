272a6c217bc99053661d230ec42728833bb75f86
BrunoSSantana <brunoosouza15@gmail.com>
2021-11-22 20:30:41 -0300

    fix import entity

--- src/repositories/in-memory/MentorsRepositoryInMemory.ts
+++ src/repositories/in-memory/MentorsRepositoryInMemory.ts
@@ -1,7 +1,7 @@
-import { Mentored } from 'entities/Mentored'
 import { v4 as uuid } from 'uuid'
 
 import { Mentor } from '../../entities/Mentor'
+import { Mentored } from '../../entities/Mentored'
 import { IMentorsRepositories } from '../../repositories/IMentorsRepositories'
 
 class MentorsRepositoryInMemory implements IMentorsRepositories {
